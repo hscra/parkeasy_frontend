b43badab330ae0dc464b7e121d0f41c6f01f84ca
Jan Gwara <janekgwara13@gmail.com>
2024-11-24 20:25:57 +0100

    signup

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -7,11 +7,15 @@ const Register: React.FC = () => {
   return (
     <div id="home" className="flex justify-center align-center">
       <h1 className="justify-center align-center">Sign Up</h1>
+      <div>
         <TextField id="outlined-basic" label="Email" variant="outlined" />
+      </div>
+      <div className="position-fixed">
         <TextField id="outlined-basic" label="Password" variant="outlined" />
-        <Button variant="contained" color="primary">
-          Sign In
-        </Button>
+      </div>
+      <Button variant="contained" color="primary">
+        Sign In
+      </Button>
     </div>
   );
 };
