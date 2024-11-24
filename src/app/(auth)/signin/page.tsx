de0ebe945dcc498dee0188f3d4a31df719287543
Jan Gwara <janekgwara13@gmail.com>
2024-11-24 21:25:40 +0100

    login and registration forms done - no DB connection - just visual

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -1,14 +1,30 @@
 import React from "react";
-import { useState } from "react";
-import { TextField, Button } from "@mui/material";
-import { useRouter } from "next/navigation";
+import { TextField, Button, Box } from "@mui/material";
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import Link from '@mui/material/Link';
 
 const Login: React.FC = () => {
-    return (
-        <div id="home" className="flex justify-center align-center">
-            <div>Sign In</div>
-        </div>
-    );
+  return (
+    <div id="home" className="flex justify-center align-center">
+      <Card sx={{ minWidth: 275, minHeight: 200, padding: 2 }}>
+      <CardContent>
+      <Typography gutterBottom sx={{ color: 'text.primary', fontSize: 24}} align="justify">Welcome Back!</Typography>
+      <Typography gutterBottom sx={{ color: 'text.secondary', fontSize: 20}} align="justify">Sign In</Typography>
+          <TextField sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: 2}} id="email" label="Email" variant="outlined" type="email"/>
+          <TextField sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: 2}}id="password" label="Password" variant="outlined" type="password" />
+          <Typography sx={{ color: 'text.secondary', fontSize: 12}} align="justify"><a href="/login">Don't have an account yet?</a> <Link href="/signup">Register Now!</Link></Typography>
+      </CardContent>
+      <CardActions>
+          <Button variant="contained" color="primary">
+            Sign In
+          </Button>
+      </CardActions>
+      </Card>
+    </div>
+  );
 };
 
 export default Login;
--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,21 +1,28 @@
 import React from "react";
-import { useState } from "react";
-import { TextField, Button } from "@mui/material";
-import { useRouter } from "next/navigation";
+import { TextField, Button, Box } from "@mui/material";
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import Link from '@mui/material/Link';
 
 const Register: React.FC = () => {
   return (
     <div id="home" className="flex justify-center align-center">
-      <h1 className="justify-center align-center">Sign Up</h1>
-      <div>
-        <TextField id="outlined-basic" label="Email" variant="outlined" />
-      </div>
-      <div className="position-fixed">
-        <TextField id="outlined-basic" label="Password" variant="outlined" />
-      </div>
-      <Button variant="contained" color="primary">
-        Sign In
-      </Button>
+      <Card sx={{ minWidth: 275, minHeight: 200, padding: 2 }}>
+      <CardContent>
+      <Typography gutterBottom sx={{ color: 'text.primary', fontSize: 20}} align="justify">Sign Up</Typography>
+          <TextField sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: 2}} id="username" label="Username" variant="outlined" type="username"/>
+          <TextField sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: 2}} id="email" label="Email" variant="outlined" type="email"/>
+          <TextField sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: 2}}id="password" label="Password" variant="outlined" type="password" />
+          <Typography sx={{ color: 'text.secondary', fontSize: 12}} align="justify"><a href="/login">Already have an account?</a> <Link href="/signin">Log In!</Link></Typography>
+      </CardContent>
+      <CardActions>
+          <Button variant="contained" color="primary">
+            Sign Up
+          </Button>
+      </CardActions>
+      </Card>
     </div>
   );
 };
