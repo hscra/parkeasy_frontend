245313f3e6d96a3368fcc788a07257b8338f1707
Jan Gwara <janekgwara13@gmail.com>
2024-11-24 21:51:27 +0100

    logo done!!!

--- src/app/components/Logo.tsx
+++ src/app/components/Logo.tsx
@@ -1,14 +1,16 @@
+'use client';
+
 import React from "react";
 import Image from 'next/image';
-
+import logo from '../public/logo.jpg';
 
 const Header: React.FC = () => {
     return (
-        <div id="logo" className="h-[100%] w-[20%] flex justify-around items-center">
-            <div id="picture">
-                <Image src='/logo.jpg' alt="Logo" width={100} height={100} />
+        <div id="logo" style={{ height: '100%', width: '20%', display: 'flex', justifyContent: 'flex-start', alignItems: 'left' }}>
+            <div id="picture" style={{ display: 'flex', alignItems: 'center' }}>
+                <Image src={logo} alt="Logo" width={100} height={100} />
+                <a style={{ marginLeft: '8px' }} href = "/app">ParkEasy</a>
             </div>
-            <div id="app-name">ParkEasy</div>
         </div>
     );
 };
---
+++ src/app/home/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HomePage: React.FC = () => {
+  return (
+    <div id="home" className="flex justify-center align-center">
+      <div>Home Page</div>
+    </div>
+  );
+};
+
+export default HomePage;
