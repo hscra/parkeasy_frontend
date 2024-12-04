151f3885edb62db175591440e3fbd1558a4c6979
rjodlowski <rafaljodlowski24@gmail.com>
2024-12-04 01:58:20 +0100

    Google maps adjustments

---
+++ src/app/components/Map.tsx
@@ -0,0 +1,67 @@
+"use client";
+
+import React, { MutableRefObject, useEffect, useState } from "react";
+import { Location } from "../page";
+import { Loader } from "@googlemaps/js-api-loader";
+
+type MapProps = {
+  locations: Location[]
+}
+
+const Map: React.FC<MapProps> = ({ locations }) => {
+  const markersRef = React.useRef<google.maps.marker.AdvancedMarkerElement[]>([]);
+  const mapRef = React.useRef<HTMLDivElement | null>(null);
+  const mapInstanceRef = React.useRef<google.maps.Map | null>(null)
+  const [mapReady, setMapReady] = useState<boolean>(false)
+
+  useEffect(() => {
+    const initMap = async () => {
+      // Load all subscomponents
+      const loader = new Loader({
+        apiKey: process.env.NEXT_PUBLIC_MAPS_API_KEY as string,
+        version: "weekly",
+        libraries: ["maps", "marker"],
+      });
+      const { Map } = await loader.importLibrary("maps");
+      const { AdvancedMarkerElement } = (await loader.importLibrary(
+        "marker"
+      )) as google.maps.MarkerLibrary;
+      // Create map
+      const mapOptions: google.maps.MapOptions = {
+        center: { lat: 52.191097, lng: 19.355406 },
+        zoom: 6,
+        mapId: "MY_NEXTJS_MAPID",
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      };
+
+      // Save the reference for future use
+      mapInstanceRef.current = new Map(mapRef.current as HTMLDivElement, mapOptions);
+      setMapReady(true)
+    };
+
+    initMap();
+  }, []);
+
+  useEffect(() => {
+    if (!mapReady || !mapInstanceRef.current) return;
+
+    // Clear existing markers
+    markersRef.current.forEach((marker) => marker.map = null);
+    markersRef.current = [];
+
+    // Set them again
+    markersRef.current = locations.map((location) => {
+      const markerElement = new google.maps.marker.AdvancedMarkerElement({
+        map: mapInstanceRef.current,
+        position: location.position,
+      });
+      return markerElement;
+    });
+  }, [locations, mapReady]);
+
+  return (
+    <div id="map" style={{ height: "600px" }} ref={mapRef}></div>
+  );
+}
+
+export default Map;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,61 @@
-import Image from "next/image";
-import { Map } from "./map";
-import React from "react";
+"use client";
 
-export default function Home() {
+import Map from "./components/Map";
+import React, { useEffect, useState } from "react";
+
+export type Location = {
+  id: number,
+  city: string,
+  position: Position
+}
+export type Position = {
+  lat: number,
+  lng: number
+}
+
+const Home: React.FC = () => {
+  const [locations, setLocations] = useState([])
+
+  const getLocations = async () => {
+    const apiUrl = "http://localhost:8080/location/getAllLocations";
+    try {
+      const response = await fetch(apiUrl, {
+        method: "GET",
+        credentials: "include",
+      });
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const data = await response.json();
+
+      setLocations(data.map((el: any) => {
+        return {
+          id: el.id,
+          city: el.city,
+          position: {
+            lat: el.lat,
+            lng: el.lng,
+          } as Position
+        } as Location
+      }))
+    } catch (error) {
+      console.error("Error to fetching location data", error);
+    }
+  }
+
+  useEffect(() => {
+    getLocations();
+  }, [])
+
   return (
     <main className="sm:container">
-      {/* search form */}
-      <h1>Search form</h1>
-
       <div className="min-h-full">
-        <main>
-          <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-            <h1 className="text-black">Home</h1>
-            <Map />
-            {}
-          </div>
-        </main>
+        <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+          <Map locations={locations} />
+        </div>
       </div>
     </main>
   );
 }
+
+export default Home;
