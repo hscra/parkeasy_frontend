889d46366644489ea88783bc6b4b2e316f148a9f
rjodlowski <rafaljodlowski24@gmail.com>
2025-01-08 01:07:59 +0100

    Fetch spaces for location

--- src/app/components/Detail.tsx
+++ src/app/components/Detail.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Table from '@mui/joy/Table';
 import Typography from '@mui/joy/Typography';
 import Sheet from '@mui/joy/Sheet';
@@ -6,39 +6,48 @@ import Sheet from '@mui/joy/Sheet';
 export type ParkingDetailsProps = {
   id_location: number;
 };
+export type ParkingSpace = {
+  id: number;
+  city_id: number;
+  availability: boolean;
+}
 
 const Detail: React.FC<ParkingDetailsProps> = ({id_location}) => {
-    // const fetchLocationDetails = async (ids: number[]): Promise<ParkingDetailsProps[]> => {
-    //   try {
-    //     const detailPromises = ids.map(async (id) => {
-    //       const response = await fetch(
-    //         `http://localhost:8080/locations/get?Id=${id}`,
-    //         {
-    //           method: "GET",
-    //           headers: {
-    //             "Content-Type": "application/json",
-    //           },
-    //           credentials: "include",
-    //         }
-    //       );
-    //       if (!response.ok) {
-    //         throw new Error(
-    //           `Failed to fetch details for location ${id}: ${response.statusText}`
-    //         );
-    //       }
-    //       const rawData = await response.json();
-    //       return rawData;
-    //     });
-    //     return Promise.all(detailPromises);
-    //   } catch (err: any) {
-    //     setError(err.message || "Failed to fetch location details");
-    //     return [];
-    //   }
-    // }
+  const [parkingSpaces, setParkingSpaces] = React.useState<ParkingSpace[]>([])
+
+  const fetchParkingSpaces = async () => {
+    if (id_location === 0) return;
+
+    const apiUrl = "http://localhost:8080/parking/getAllInCity?Id=" + id_location;
+    try {
+      const response = await fetch(apiUrl, {
+        method: "GET",
+        credentials: "include",
+      });
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const data = await response.json();
+      console.log(data)
+
+      setParkingSpaces(data.map((el: any) => {
+        return {
+          id: el.id,
+          city_id: el.city_Id,
+          availability: el.availability,
+        } as ParkingSpace
+      }))
+    } catch (error) {
+      console.error("Error to fetching location data", error);
+    }
+  }
+
+  useEffect(() => {
+    fetchParkingSpaces()
+  }, [id_location])
 
   return (
     <>
-      {id_location}
       <Sheet>
         <Table
         borderAxis="xBetween"
@@ -47,7 +56,24 @@ const Detail: React.FC<ParkingDetailsProps> = ({id_location}) => {
         stickyFooter
         stickyHeader
         variant="soft"
-      />
+        >
+          <thead>
+            <tr>
+              <th>Row (id)</th>
+              <th>city_id</th>
+              <th>Availability</th>
+            </tr>
+          </thead>
+          <tbody>
+            {parkingSpaces.map((ps) => (
+              <tr key={ps.id}>
+                <td>{ps.id}</td>
+                <td>{ps.city_id}</td>
+                <td>{(ps.availability).toString()}</td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
       </Sheet>
     </>
   );
