5b2e726adfa0c415bba6c25bb8dba13cf7c30a59
rjodlowski <rafaljodlowski24@gmail.com>
2025-01-13 16:05:24 +0100

    Small adjustments

--- src/app/components/DetailParkingSpaceRow.tsx
+++ src/app/components/DetailParkingSpaceRow.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { ParkingSpaceDataItem } from "./Detail";
 
 export type DetailParkingSpaceRowProps = {
   id: number;
@@ -10,7 +9,7 @@ export type DetailParkingSpaceRowProps = {
 
 const DetailParkingSpaceRow: React.FC<DetailParkingSpaceRowProps> = ({id, city_id, availability, onClick}) => {
   return (
-    <tr key={id} onClick={() => onClick(id)}>
+    <tr key={id} onClick={() => onClick(id)} className="cursor-pointer">
       <td>{id}</td>
       <td>{city_id}</td>
       <td>{(availability).toString()}</td>
