a2f0ccb8908813ef97c3ff49da7484a42d58fec1
rjodlowski <rafaljodlowski24@gmail.com>
2025-02-11 22:14:34 +0100

    Show historical reservations

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -14,6 +14,7 @@ export type Reservation = {
   startTime: string;
   place: string;
   paymentStatus: number;
+  parkingSpaceId: number;
   points: number;
   position: Position;
 };
@@ -27,7 +28,7 @@ const Orders: React.FC = () => {
   const [locations, setLocations] = useState<Location[]>([]);
   const [user, setUser] = useState<UserData>({} as UserData);
   const [userReservations, setUserReservations] = useState<Reservation[]>([]);
-  // const [selectedSpace, setSelectedSpace] = useState<number>(0);
+  const [userHistoricalReservations, setUserHistoricalReservations] = useState<Reservation[]>([]);
   const router = useRouter()
 
   const fetchUser = async () => {
@@ -101,6 +102,7 @@ const Orders: React.FC = () => {
           startTime: el.startTime,
           place: el.place,
           paymentStatus: el.paymentStatus,
+          parkingSpaceId: el.parkingSpaceId,
           points: el.points,
           position: {
             lat: el.lat,
@@ -110,6 +112,43 @@ const Orders: React.FC = () => {
       );
     } catch (error) {
         console.error("Error fetching reservation data", error);
+    }
+  };
+
+  const getUserHistoricalReservations = async (id: number) => {
+    const apiUrl = process.env.SERVER_DOMAIN + "/historical_reservation/getUserReservations?Id=" + (id ?? 0);
+    try {
+      const response = await fetch(apiUrl, {
+        method: "GET",
+        credentials: "include",
+      });
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+
+      const data = await response.json();
+      console.log(data)
+
+      setUserHistoricalReservations(
+        data.map((el: any) => ({
+          id: el.reservationId,
+          userId: el.userId,
+          endTime: el.endTime,
+          startTime: el.startTime,
+          place: el.place,
+          paymentStatus: el.paymentStatus,
+          parkingSpaceId: el.parkingSpaceId,
+          points: el.points,
+          position: {
+            lat: el.lat,
+            lng: el.lng,
+          } as Position,
+        } as Reservation))
+      );
+    } catch (error) {
+        console.error("Error fetching reservation history data", error);
     }
   };
 
@@ -134,7 +173,6 @@ const Orders: React.FC = () => {
         }
 
         let res = await response.json();
-        console.log(res);
 
         if (res.status === "SUCCESS") {
           console.log("Payment successful!");
@@ -183,6 +221,7 @@ const Orders: React.FC = () => {
       const userData = await fetchUser();
       if (userData?.id) {
         await getUserReservations(userData.id);
+        await getUserHistoricalReservations(userData.id);
       }
       getLocations();
     };
@@ -195,33 +234,6 @@ const Orders: React.FC = () => {
     // setSelectedLocation(id);
     // setSelectedSpace(0);
   };
-
-  // Mock data for reservation history (Past reservations)
-  // const reservationHistory: Reservation[] = [
-  //   {
-  //     id: 4,
-  //     time: "2024-12-15 18:30",
-  //     place: "Eastside Parking",
-  //     paymentStatus: "Awaiting Payment",
-  //     position: { lat: 48.8566, lng: 2.3522 }, // Example coordinates (Paris)
-  //   },
-  //   {
-  //     id: 5,
-  //     time: "2024-11-05 13:00",
-  //     place: "Westfield Mall Parking",
-  //     paymentStatus: "Paid",
-  //     position: { lat: 37.7749, lng: -122.4194 }, // Example coordinates (San Francisco)
-  //   },
-  //   {
-  //     id: 6,
-  //     time: "2024-09-10 11:45",
-  //     place: "Airport Parking",
-  //     paymentStatus: "Awaiting Payment",
-  //     position: { lat: 52.5200, lng: 13.4050 }, // Example coordinates (Berlin)
-  //   },
-  // ];
-
-  // State to track the selected reservation location
 
   //? Handle reservation click and update the map's location
   const handleReservationClick = (position: Position) => {
@@ -251,6 +263,9 @@ const Orders: React.FC = () => {
                       <strong className="text-blue-500">Place:</strong> {reservation.place}
                     </p>
                     <p className="text-2xl mb-4 text-center">
+                      <strong className="text-blue-500">Parking Space:</strong> {reservation.parkingSpaceId}
+                    </p>
+                    <p className="text-2xl mb-4 text-center">
                       <strong className="text-blue-600">Payment Status:&nbsp;</strong>
                       {reservation.paymentStatus == null ?
                         <button className="underline" onClick={() => payForReservation(reservation.id)}>Awaiting Payment</button>
@@ -274,7 +289,7 @@ const Orders: React.FC = () => {
         </div>
 
         {/* Reservation History Section Below Reservations */}
-        {/* <div className="w-full px-8 md:px-16 xl:px-32 py-8 mt-12 rounded-lg shadow-lg">
+        <div className="w-full px-8 md:px-16 xl:px-32 py-8 mt-12 rounded-lg shadow-lg">
           <h2 className="text-3xl font-semibold mb-4 text-blue-600">Reservation History</h2>
           <table className="min-w-full table-auto">
             <thead>
@@ -282,21 +297,23 @@ const Orders: React.FC = () => {
                 <th className="py-2 px-4 text-left text-lg font-medium text-gray-700">Start Time</th>
                 <th className="py-2 px-4 text-left text-lg font-medium text-gray-700">End Time</th>
                 <th className="py-2 px-4 text-left text-lg font-medium text-gray-700">Place</th>
+                <th className="py-2 px-4 text-left text-lg font-medium text-gray-700">Parking Space</th>
                 <th className="py-2 px-4 text-left text-lg font-medium text-gray-700">Payment Status</th>
               </tr>
             </thead>
             <tbody>
-              {reservationHistory.map((reservation) => (
+              {userHistoricalReservations.map((reservation) => (
                 <tr key={reservation.id} className="border-b">
                   <td className="py-4 px-4 text-lg text-gray-700">{reservation.startTime}</td>
                   <td className="py-4 px-4 text-lg text-gray-700">{reservation.endTime}</td>
                   <td className="py-4 px-4 text-lg text-gray-700">{reservation.place}</td>
-                  <td className="py-4 px-4 text-lg text-gray-700">{reservation.paymentStatus}</td>
+                  <td className="py-4 px-4 text-lg text-gray-700">{reservation.parkingSpaceId}</td>
+                  <td className="py-4 px-4 text-lg text-gray-700">{reservation.paymentStatus === 1 ? "Paid" : "Failed"}</td>
                 </tr>
               ))}
             </tbody>
           </table>
-        </div> */}
+        </div>
       </div>
   );
 };
